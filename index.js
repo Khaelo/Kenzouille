0e87e56d98d865b8ea66604f36e4089504dc3ec4
Khaelo <luca.bruno@ynov.com>
2020-11-15 11:08:42 +0100

    Just Kenza

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ client.once('ready', () => {
     console.log('Ready!');
 
     client.user.setStatus("online");
-    client.user.setActivity("les messages de Kenza et Aurelie", {type:3})
+    client.user.setActivity("les messages de Kenza", {type:3})
 });
 
 client.on('message', (message) => {
     if (message.author.id === "766652908669632542") {
         const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'Kenzouille');
-        message.react(reactionEmoji);
-    }
-    if (message.author.id === "688786329361973256") {
-        const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'Aurelius');
         message.react(reactionEmoji);
     }
 });
